93e65f59097d372492af0b13d9a23270dc8f30e0
healthybamboo <healthybamboo@bamgrove.net>
2025-05-13 20:34:54 +0900

    actionを追加

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # create_release_note
 .versionファイルからrelease_noteを作成する
+
+
+## 参考
+- https://github.com/actions/toolkit
---
+++ index.ts
@@ -0,0 +1,62 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+import * as fs from "fs";
+
+
+/**
+ * tagを更新リクエストのtypeです。
+ */
+type TagObject = RestEndpointMethodTypes["git"]["createTag"]["parameters"];
+
+/**
+ * Githb Actionsで .versionファイルからバージョンを
+ */
+async function run() {
+  try {
+    // .versionファイルから現在のバージョンを取得
+    const workspace = process.env.GITHUB_WORKSPACE || process.cwd();
+    const dotVersionFile = core.getInput("dotnet-version-path") || ".version";
+    const dotVersionFilePath = `${workspace}/${dotVersionFile}`;
+
+    const version = fs.readFileSync(dotVersionFilePath, "utf8").trim();
+    core.info(`Current version: ${version}`);
+
+    // Githubのタグを更新
+    const octokit = github.getOctokit(core.getInput("github-token"));
+    const { owner, repo } = github.context.repo;
+    const tagName = `v${version}`;
+    const tagMessage = `Release version ${version}`;
+    const tagObject: TagObject = {
+      owner,
+      repo,
+      tag: tagName,
+      message: tagMessage,
+      object: github.context.sha,
+      type: "commit",
+    };
+    const tagResponse = await octokit.rest.git.createTag(tagObject)
+    core.info(`Tag created: ${tagResponse.data.tag}`);
+
+    // タグを元にリリースノートを作成
+    const releaseResponse = await octokit.rest.repos.createRelease({
+      owner,
+      repo,
+      tag_name: tagName,
+      name: tagName,
+      body: `Release version ${version}`,
+    });
+    core.info(`Release created: ${releaseResponse.data.html_url}`);
+    core.info(`Release URL: ${releaseResponse.data.html_url}`);
+
+    // 処理が成功したことを通知
+    core.info(`Version ${version} has been released successfully.`);
+    core.setOutput("tag", tagName);
+    core.setOutput("release", releaseResponse.data.html_url);
+    core.setOutput("version", version)
+  } catch (error: any) {
+    core.error(`Error reading version file: ${error.message}`);
+    core.setFailed(error.message);
+  }
+}
+run();
